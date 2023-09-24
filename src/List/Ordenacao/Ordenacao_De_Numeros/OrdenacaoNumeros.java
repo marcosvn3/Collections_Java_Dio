29e91c412b6c9df33c37ad1c4d46d40fc5427563
Marcos Santos <marcosvn3@academico.ufs.br>
2023-09-24 00:19:27 -0300

    Tarefa incompleta!!

---
+++ src/List/Ordenacao/Ordenacao_De_Numeros/OrdenacaoNumeros.java
@@ -0,0 +1,38 @@
+package List.Ordenacao.Ordenacao_De_Numeros;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoNumeros implements Comparable<Integer> {
+    List<Integer>listNum = new ArrayList<>();
+
+    public OrdenacaoNumeros() {
+        this.listNum = new ArrayList<>();
+    }
+
+
+    public void adicionarNum(int num){
+        listNum.add(num);
+    }
+
+    public List<Integer> ordenarAscendente(){
+        List<Integer>ordenada = new ArrayList<>(listNum);
+        Collections.sort(ordenada);
+
+        return ordenada;
+    }
+
+    public List<Integer> ordenarDescendente(){
+        List<Integer>ordenada = new ArrayList<>(listNum);
+
+        return ordenada;
+    }
+
+
+    @Override
+    public int compareTo(Integer o) {
+        return Integer.compare(listNum.get(0), o.intValue() );
+    }
+}
