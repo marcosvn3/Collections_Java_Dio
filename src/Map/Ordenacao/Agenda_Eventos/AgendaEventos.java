85f0b9b0ebeed6611c05a68ef1e739f044028b4b
Marcos Santos <marcosvn3@academico.ufs.br>
2023-09-27 20:36:49 -0300

    Tarefa AgendaEvento Completa!

---
+++ src/Map/Ordenacao/Agenda_Eventos/AgendaEventos.java
@@ -0,0 +1,39 @@
+package Map.Ordenacao.Agenda_Eventos;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nomeEvento, String descricaoAtracao) {
+        eventosMap.put(data, new Evento(nomeEvento, descricaoAtracao));
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            Evento evento = entry.getValue();
+            System.out.println("Data: " + dataEvento + ", Evento: " + evento.getNome() + ", Atração: " + evento.getAtracao());
+        }
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximaData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+    }
+}
